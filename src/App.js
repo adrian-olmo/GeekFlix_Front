2d730ce73f3217296f67e0c66de906fc91e38e6b
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-04-24 18:01:55 +0200

    Diseño responsive header & footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { Footer } from './components/footer/Footer';
+import { Header } from './components/header/Header';
 import Login from './containers/login/Login';
 import Signup from './containers/signup/Signup';
 
@@ -6,9 +8,13 @@ function App() {
   return (
     <div className="bg">
 
+      <Header />
+
+
+
       <Signup></Signup>
       <Login></Login>
-
+      <Footer />
     </div>
   );
 }
