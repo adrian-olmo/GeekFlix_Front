0a5ba124ce33d03798b04b2ef8142d596d5ffae1
biorubenfs <biorubenfs@hotmail.com>
2021-04-28 17:23:35 +0200

    Minor problems were fixed in responsive

--- src/components/cardUser/CardUser.js
+++ src/components/cardUser/CardUser.js
@@ -32,13 +32,13 @@ const CardUser = () => {
 
             <div className="display-orders">
                 <h2 className="titulo"><strong>Tus pedidos</strong></h2>
-                <div className="field-name">
-                    <div></div>
+                <div className="field-names">
+                    <div className="no-color-text">PosterPath</div>
                     <div>Titulo</div>
                     <div>Inicio alquiler</div>
                     <div>Final alquiler</div>
                 </div>
-                {orders.map(order => <CardOrder key={orders.indexOf(order)} title={order.title} start={order.orderStart} end={order.orderEnd} poster={order.poster}></CardOrder>)}
+                {orders.map(order => <CardOrder key={orders.indexOf(order)} title={order.title} start={order.orderStart.substring(0, 10)} end={order.orderEnd.substring(0, 10)} poster={order.poster}></CardOrder>)}
             </div>
         </div>
 
